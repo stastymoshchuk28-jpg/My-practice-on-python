64cd1191b4d058a44923dfff6d4a06a296d657a9
Stanislav <stastymoshchuk28@gmail.com>
2026-07-05 14:03:03 +0300

    Upgrade

--- Python/new_proggram.py
+++ Python/new_proggram.py
@@ -1,1 +1,22 @@
 #Not done!
+import time
+
+while True:
+    print("Choose what to do:")
+    print("1. Write new text and analyze it!")
+    print("2. Exit the proggram")
+    choose_do = input("Choose number of action: ")
+    if choose_do.isalpha():
+        print("Choose can't be letter! Try again!")
+        continue
+    elif choose_do.isdigit():
+        choose_do = int(choose_do)
+        if choose_do == 1:
+            text = input("Okay, firstly write the text: ")
+        elif choose_do == 2:
+            print("Thank's for using proggram!")
+            print("Goodbye!")
+            break
+        else:
+            print("Not right number! Try again!")
+            continue
