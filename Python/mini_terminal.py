cedfee05880273fe035a1c7d7dc52305bce2e8e5
Stanislav <stastymoshchuk28@gmail.com>
2026-09-05 20:02:48 +0300

    Done mini_terminal

--- Python/mini_terminal.py
+++ Python/mini_terminal.py
@@ -1,7 +1,9 @@
-#Not Done
-#v0.5
+#Done
+#v1.0
 
-from time import sleep, ctime
+from time import sleep, ctime, time
+from random import choice, randint
+from turtle import *
 
 def pause_long():
     print(" ")
@@ -23,6 +25,7 @@ def show_error(errors:dict, error_value:str, command=""):
     pause_long()     
     
 def start_work(user_info:dict, errors:dict):
+    start_time = time()
     while True:
         print("=== Welcome to Myrchik OS! ===")
         pause_long()
@@ -84,7 +87,7 @@ def start_work(user_info:dict, errors:dict):
                 pause_long()
                 print("Enter command 'help' to see commands list or 'stop' to end your work!")
                 pause_long()                
-            return user_info
+            return user_info, start_time
 
 def command_list_show(user_info:dict, commands:dict):
     print("Commands list: ")
@@ -125,6 +128,12 @@ def show_time():
     print(f"Current time: {current_time}")
     pause_long()
 
+def using_time(start_time):
+    now_time = time()
+    time_using_terminal = now_time - start_time
+    print(f"You using Myrchik OS for {time_using_terminal:.2f} seconds!")
+    pause_long()
+
 def make_super_user(user_info:dict):
     if user_info["user_name"] is not None:
         if user_info["super_user"]:
@@ -150,6 +159,97 @@ def make_standart_user(user_info:dict):
     else:
         print("USER: USER_INFO -- user_name = None: NOT FOUND USER!")
     return user_info
+
+def coinflip():
+    coins_variants = ["eagle", "tails"]
+    print(f"Coin flip!\nYour roll: {choice(coins_variants)}")
+    pause_long()
+
+def roll(command:str):
+    the_choice_analyze = command.split()
+    if len(the_choice_analyze) == 1:
+        the_number = randint(1, 6)
+        print(f"Cube roll!\nRoll: {the_number}")
+        pause_long()
+    else:
+        value = the_choice_analyze[1]
+        if value.isdigit():
+            value = int(value)
+            the_number = randint(1, value)
+            print(f"Cube roll!\nRoll: {the_number}")
+            pause_long()
+        else:
+            the_number = randint(1, 6)
+            print(f"Cube roll!\nRoll: {the_number}")
+            pause_long()        
+
+def show_history(history:list):
+    for num, c in enumerate(history):
+        print(f"{num}. '{c}'")
+
+def rename(user_info:dict, errors:dict):
+    while True:
+        new_user_name = input("Enter username for account: ").strip()
+        pause_long()
+        if new_user_name == "":
+            show_error(errors, "empty_user_name")
+            continue
+        else:
+            print("INFO-USER.XXX-NEW_USERNAME.SAVED")
+            pause_long()
+            user_info["user_name"] = new_user_name
+            break
+    return user_info
+
+def changepass(user_info:dict, errors:dict):
+    while True:
+        password = input("Enter password for your system: ").strip()    
+        pause_long()     
+        if password == "":
+            show_error(errors, "empty_password")  
+            continue
+        else:
+            print(f"Password {password} saved!")
+            pause_long()
+            user_info["password"] = password    
+            break
+    return user_info
+
+def guess_number():
+    number_to_guess = randint(1, 100)
+    while True:
+        try:
+            user_guess = int(input("Enter a number to guess: "))
+            pause_long()
+        except ValueError:
+            print("Enter a number!")
+            pause_long()
+            continue
+        if user_guess < number_to_guess:
+            print("Number to guess is bigger!")
+            pause_long()
+            continue
+        elif user_guess > number_to_guess:
+            print("Number to guess is lower!")
+            pause_long()
+            continue
+        else:
+            print("You win!")
+            pause_long()
+            break
+
+def draw_square():
+    speed(10000000000)
+    penup()
+    goto(-50, 50)
+    pendown()
+    color("red")
+    width(10)
+    for line in range(0, 4, 1):
+        fd(100)
+        rt(90)
+    print("Square drawed")
+    pause_long()
 
 def delete_user(user_info:dict, errors:dict):
     if user_info["user_name"] is not None:
@@ -189,10 +289,11 @@ def delete_user(user_info:dict, errors:dict):
         pause_long()
         return user_info
         
-def stop_work():
+def stop_work(user_info:dict, start_time):
     print("Thanks for using Myrchik OS!")
     pause_long()
-    print("Goodbye!")
+    using_time(start_time)
+    print(f"Goodbye, {user_info['user_name']}!")
     pause_long()
     exit()
 
@@ -207,7 +308,15 @@ commands = {
         "help": lambda: command_list_show(user_info, commands),
         "whoami": lambda: show_user(user_info),
         "time": show_time,
-        "stop": stop_work,
+        "using_time": lambda: using_time(start_time),
+        "coinflip": coinflip,
+        "roll": lambda: roll(command),
+        "stop": lambda: stop_work(user_info, start_time),
+        "history": lambda: show_history(history),
+        "rename": lambda: rename(user_info, errors),
+        "changepass": lambda: changepass(user_info, errors),
+        "guess_number_game": guess_number,
+        "draw_square": draw_square,
         "sudo": lambda: make_super_user(user_info),
         "de_sudo": lambda: make_standart_user(user_info)
     },
@@ -259,7 +368,9 @@ errors = {
         },
 }
 
-user_info = start_work(user_info, errors)
+history = []
+
+user_info, start_time = start_work(user_info, errors)
 
 while True:
     command = input("Enter name of command ('help' to see commands list): ").strip().lower()
@@ -270,19 +381,25 @@ while True:
             command_to_enter()      
         else:
             show_error(errors, "no_user_error", command)
-            user_info = start_work(user_info, errors)
+            user_info, start_time = start_work(user_info, errors)
             continue   
     else:
-        if command in commands["standart_user_commands"]:
-            command_to_enter = commands["standart_user_commands"][command]
-            command_to_enter()           
-        elif command in commands["super_user_commands"] and user_info["super_user"]:
-            command_to_enter = commands["super_user_commands"][command]
+        the_comm = command.split()
+        command_base = the_comm[0]
+        if command_base in commands["standart_user_commands"]:
+            command_to_enter = commands["standart_user_commands"][command_base]
+            command_to_enter() 
+            history.append(command)
+        elif command_base in commands["super_user_commands"] and user_info["super_user"]:
+            command_to_enter = commands["super_user_commands"][command_base]
             command_to_enter()
+            history.append(command)
         else:
             if not command in commands["standart_user_commands"] and not command in commands["super_user_commands"]:
                 show_error(errors, "command_not_found", command)
+                history.append(f"Error command: {command}")
                 continue
             elif command in commands["super_user_commands"] and not user_info["super_user"]:
                 show_error(errors, "command_blocked", command)
+                history.append(f"Error command: {command}")
                 continue
